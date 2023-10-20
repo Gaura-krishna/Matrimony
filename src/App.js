b4b139704bf4bef2c1a3d912d562b14605f83df3
Gaura-krishna <gaurakrishna31@gmail.com>
2023-10-20 14:26:42 +0530

    removed hello app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> hello + world</h1>
+        
+        <p>this is my page</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
